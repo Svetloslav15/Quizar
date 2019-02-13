fcd902fdf675f341bf1b9fed8bc4f86bc98c6808
Svetloslav15 <sn_novoselski@abv.bg>
2019-02-13 16:33:22 +0200

    fix bugs

--- server/config/settings.js
+++ server/config/settings.js
@@ -3,14 +3,14 @@ const path = require('path')
 let rootPath = path.normalize(path.join(__dirname, '/../../'))
 
 module.exports = {
-  development: {
-    rootPath: rootPath,
-    db: 'mongodb://localhost:27017/quizar',
-    port: 3000
-  },
-  staging: {
-  },
-  production: {
-    port: process.env.PORT
-  }
+    development: {
+        rootPath: rootPath,
+        db: 'mongodb://localhost:27017/quizar',
+        port: 3000
+    },
+    staging: {},
+    production: {
+        port: process.env.PORT,
+        db: 'mongodb://Svetloslav:Be4Mg12Ca20Sr38@ds127624.mlab.com:27624/quizardb'
+    }
 };
